41abf6ad4991de18c77c60a92fdefb1bd4bea5a3
jaxels10 <jaxels10@student.aau.dk>
2019-10-10 22:04:21 +0200

    first commit

---
+++ TestCNN/TestCNN.py
@@ -0,0 +1,17 @@
+from keras.models import load_model
+import numpy as np
+
+classifier = load_model('model.h5')
+
+from keras.preprocessing import image
+
+# Insert image path below for the image you want to test on the neural network
+
+test_image = image.load_img('INSERT IMAGE PATH HERE', target_size=(64, 64))
+test_image = image.img_to_array(test_image)
+test_image = test_image/255
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict_proba(test_image)
+
+
+print(result[0])
